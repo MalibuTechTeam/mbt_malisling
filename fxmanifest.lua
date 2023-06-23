62f550bf3f6e6d40538745c3e49b45047246dc41
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2023-06-23 13:56:27 +0000

    chore: bump manifest version to v1.1.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 name 'mbt_malisling'
 author 'Malibù Tech Team'
-version      '1.1.1'
+version      '1.1.2'
 repository 'https://github.com/MalibuTechTeam/mbt_malisling'
 description 'Weapon on back with various features'
 
