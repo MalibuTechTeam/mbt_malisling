4602365aa8e9ee455038d7351eeadf64cb9d431a
Malibú Tech Team <80293723+MalibuTechTeam@users.noreply.github.com>
2023-07-08 02:34:40 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ MBT.Debug = false
 MBT.DropWeaponOnDeath = true
 MBT.EnableSling = true
 MBT.EnableFlashlight = true
-MBT.Relog = true -- Put this to true if you have a esx_multicharacter and relog enabled!
+MBT.Relog = false -- Put this to true if you have a esx_multicharacter and relog enabled!
 
 MBT.Jamming = {
     ["Enabled"] = true,
